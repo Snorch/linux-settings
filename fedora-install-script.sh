391e61719b585982b81f9c017ea5a8e7330c385e
Pavel Tikhomirov <ptikhomirov@parallels.com>
2014-07-17 10:20:11 +0400

    fedora install & vim settings

---
+++ fedora-install-script.sh
@@ -0,0 +1,13 @@
+sudo yum update
+sudo yum install terminator
+sudo yum install vim
+sudo yum -y install git
+sudo yum -y install svn
+sudo yum -y install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo yum -y install http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
+sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+sudo yum install flash-plugin -y
+sudo yum install cscope
+sudo yum install ctags-etags.x86_64
+sudo yum install gcc
+sudo yum install man-pages
---
+++ vimrc
@@ -0,0 +1,9 @@
+filetype indent plugin on
+
+set list
+set listchars=tab:▸\
+set tabstop=2
+set shiftwidth=2
+inoremap jk <esc>
+
+hi Search cterm=NONE ctermfg=black
